db86c0ce8596b8ecb165138187462071f5e5a9ef
Chris Matthews <chris@matthews.digital>
2025-02-02 00:57:11 +0000

    Initial Commit

---
+++ CremaTests/CremaTests.swift
@@ -0,0 +1,17 @@
+//
+//  CremaTests.swift
+//  CremaTests
+//
+//  Created by Chris Matthews on 02/02/2025.
+//
+
+import Testing
+@testable import Crema
+
+struct CremaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
